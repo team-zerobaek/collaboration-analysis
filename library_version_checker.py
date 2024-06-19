dec3b2ca877d147e96875a5786eb94bfcc6738ee
suntisfied <147501927+suntisfied@users.noreply.github.com>
2024-06-19 21:42:06 +0900

    feat: add a version checker of libraries used

---
+++ library_version_checker.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import networkx as nx
+import numpy as np
+import matplotlib as plt
+import seaborn as sns
+import sys
+
+print("Python version:", sys.version)
+print("pandas version:", pd.__version__)
+print("networkx version:", nx.__version__)
+print("numpy version:", np.__version__)
+print("matplotlib version:", plt.__version__)
+print("seaborn version:", sns.__version__)

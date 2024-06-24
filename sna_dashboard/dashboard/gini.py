f24807ec1242053d23bb80fdf7f1b0ed18d3e046
suntisfied <147501927+suntisfied@users.noreply.github.com>
2024-06-24 20:30:03 +0900

    fix: ensure to gini graph works properly

--- sna_dashboard/dashboard/gini.py
+++ sna_dashboard/dashboard/gini.py
@@ -1,7 +1,7 @@
-from dash.dependencies import Input, Output, State
+from dash import dcc, html
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
-from dash import dcc, html
 
 dash_app = None
 dataset = None
@@ -11,46 +11,41 @@ def initialize_gini_app(dash_app_instance, dataset_instance):
     dash_app = dash_app_instance
     dataset = dataset_instance
 
-    # Check if the Gini components already exist
-    existing_gini_components = [comp.id for comp in dash_app.layout.children if hasattr(comp, 'id') and comp.id in ['gini-project-dropdown-unique', 'gini-graph']]
-
-    if 'gini-project-dropdown-unique' not in existing_gini_components and 'gini-graph' not in existing_gini_components:
-        dash_app.layout.children.append(
-            html.Div([
-                html.H1("Gini Coefficient"),
-                html.Div([
-                    dcc.Dropdown(
-                        id='gini-project-dropdown-unique',
-                        options=[{'label': f'Project {i}', 'value': i} for i in dataset['project'].unique()],
-                        placeholder="Select projects",
-                        multi=True,
-                        style={'width': '200px'}
-                    ),
-                    html.Button('Reset', id='reset-gini-button', n_clicks=0)
-                ], style={'display': 'flex', 'gap': '10px', 'flexWrap': 'wrap'}),
-                dcc.Graph(id='gini-graph')
-            ], style={'margin-bottom': '20px'})
-        )
+    dash_app.layout.children.append(html.Div([
+        html.H1("Gini Coefficient"),
+        html.Div([
+            dcc.Dropdown(
+                id='gini-project-dropdown',
+                options=[{'label': f'Project {i}', 'value': i} for i in dataset['project'].unique()],
+                placeholder="Select projects",
+                multi=True,
+                style={'width': '200px'}
+            )
+        ], style={'display': 'flex', 'gap': '10px', 'flexWrap': 'wrap'}),
+        dcc.Graph(id='gini-graph')
+    ]))
 
     @dash_app.callback(
         Output('gini-graph', 'figure'),
-        [Input('gini-project-dropdown-unique', 'value')],
-        [State('gini-project-dropdown-unique', 'options')]
+        [Input('gini-project-dropdown', 'value')]
     )
-    def update_gini_graph(selected_project, options):
-        if not selected_project:
-            selected_project = [opt['value'] for opt in options]
-
+    def update_gini_graph(selected_projects):
         fig = go.Figure()
-        for project in selected_project:
+
+        if not selected_projects:
+            selected_projects = dataset['project'].unique()
+
+        for project in selected_projects:
             gini_data = dataset[dataset['project'] == project].groupby('meeting_number')['gini_coefficient'].unique().apply(lambda x: x[0])
             gini_df = pd.DataFrame({'Meeting': range(1, len(gini_data) + 1), 'Gini Coefficient': gini_data})
 
-            fig.add_trace(go.Scatter(x=gini_df['Meeting'],
-                                     y=gini_df['Gini Coefficient'],
-                                     mode='lines+markers',
-                                     name=f'Gini Coefficient Project {project}',
-                                     marker=dict(symbol='circle')))
+            fig.add_trace(go.Scatter(
+                x=gini_df['Meeting'],
+                y=gini_df['Gini Coefficient'],
+                mode='lines+markers',
+                name=f'Gini Coefficient Project {project}',
+                marker=dict(symbol='circle')
+            ))
 
         fig.update_layout(
             title='Gini Coefficient by Meeting',
@@ -60,10 +55,3 @@ def initialize_gini_app(dash_app_instance, dataset_instance):
         )
 
         return fig
-
-    @dash_app.callback(
-        Output('gini-project-dropdown-unique', 'value'),
-        [Input('reset-gini-button', 'n_clicks')]
-    )
-    def reset_gini_filters(n_clicks):
-        return []

366354f68dca0dbcf29a5289433b23288b21e443
suntisfied <147501927+suntisfied@users.noreply.github.com>
2024-06-26 17:24:06 +0900

    feat: add ml models of predicting overall and individual subjective scores

--- collaboration_analysis_api/ml/app.py
+++ collaboration_analysis_api/ml/app.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from dash import Dash, dcc, html
 from fastapi.middleware.wsgi import WSGIMiddleware
+from ml.ml_overall import initialize_overall_ml_app
+from ml.ml_individual_others import initialize_individual_others_ml_app
+from ml.ml_individual_self import initialize_individual_self_ml_app
+import pandas as pd
 
 # Initialize the FastAPI app
 fastapi_app = FastAPI()
 
 # Initialize the Dash app
 dash_app = Dash(__name__, requests_pathname_prefix='/ml/')
+
+# Load dataset
+dataset = pd.read_csv('/app/data/dataset_collaboration_with_survey_scores.csv')
 
 # Define the layout
 dash_app.layout = html.Div([
@@ -20,6 +27,11 @@ dash_app.layout = html.Div([
     html.H2("ML", style={'text-align': 'center'}),
     # Add your layout and components here for ML page
 ])
+
+# Initialize the individual ML apps
+initialize_overall_ml_app(dash_app, dataset)
+initialize_individual_others_ml_app(dash_app, dataset)
+initialize_individual_self_ml_app(dash_app, dataset)
 
 # Mount the Dash app to FastAPI using WSGIMiddleware
 fastapi_app.mount("/", WSGIMiddleware(dash_app.server))
